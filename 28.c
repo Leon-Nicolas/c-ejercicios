db55bcb6493438829b71ffdce657e9289c3818ae
Leon-Nicolas <83369007+Leon-Nicolas@users.noreply.github.com>
2021-08-18 11:47:00 -0300

    Update 28.c

--- 28.c
+++ 28.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  /* code */
+int main(int argc, char argv[]) {
+  / code */
   double xi = atoi (argv[1]);
   double vi = atoi (argv[2]);
   double t  = atoi (argv[3]);
-
-  printf("%f\n", (xi + (vi * t) + (9.8 * t)) / 2 );
+  double res = (xi + (vi * t) + (9.8 * (t * t)) / 2; 
+                
+  printf("%f\n", res);
 
   return 0;
 }
